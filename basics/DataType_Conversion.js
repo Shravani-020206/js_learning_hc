4e6fe7e11a69eb6cd87dbf269a77cf98a7107175
Shravani-020206 <145420754+Shravani-020206@users.noreply.github.com>
2023-09-20 10:39:21 +0000

    Data type conversions

---
+++ basics/DataType_Conversion.js
@@ -0,0 +1,34 @@
+let score=33
+console.log(typeof score);
+let score1="33"
+let value=Number(score1)
+console.log(value);
+console.log(typeof value);
+let score2="33abc"
+let value2=Number(score2)
+console.log(typeof value2);
+console.log(value2);
+let score3=null
+let value3=Number(score3)
+console.log(value3)
+console.log(typeof value3);
+let score4=undefined
+let value4=Number(score4)
+console.log(typeof value4)
+console.log(value4)
+let score5=true
+let value5=Number(score5)
+console.log(typeof value5);
+console.log(value5);
+let score6="shravani"
+let valueInNumber=Number(score6)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+let isLoggedIn=1
+let booleanIsloggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsloggedIn);
+console.log(booleanIsloggedIn);
+let someNumber=33
+let anotherNumber=String(someNumber)
+console.log(typeof anotherNumber)
+console.log(anotherNumber)
