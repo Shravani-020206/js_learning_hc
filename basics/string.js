87867126ee82c60ec54eb9249a8cf6c28ca0e3be
Shravani-020206 <145420754+Shravani-020206@users.noreply.github.com>
2023-09-21 17:03:28 +0000

    strings

---
+++ basics/string.js
@@ -0,0 +1,18 @@
+const name="shravani"
+const count=21
+console.log(`name is ${name} and count is ${count}`);
+const Name=new String("shravani")
+console.log(Name.charAt(2));
+console.log(Name.indexOf('a'));
+const a=Name.substring(0, 4);
+console.log(a);
+const b=Name.slice(-8, 4)
+console.log(b);
+const c=" shravani "
+console.log(c);
+console.log(c.trim());
+const url="https://shravani.com/shravani%20s"
+console.log(url.replace('%20', '-'));
+console.log(url.includes("shravani"));
+const SomeName='shravani-s-com'
+console.log(SomeName.split("-"));
