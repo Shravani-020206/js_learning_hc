ae5c6ddec9546d4a6b44c4830604a8c7d466912e
Shravani-020206 <145420754+Shravani-020206@users.noreply.github.com>
2023-09-21 16:35:37 +0000

    studied memory types

---
+++ basics/stack_and_heap.js
@@ -0,0 +1,14 @@
+let originalName="shravani"
+let nickName =originalName;
+console.log(nickName);
+ nickName="bujji"
+console.log(nickName);
+let userone={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let usertwo=userone
+usertwo.email="shravani@google.com"
+console.log(userone.email);
+console.log(usertwo.email);
+
